e269467038d1aeb0a3f6d82d206f64efb49b79d3
Jugo-JS <jugoslavsubotic@gmail.com>
2021-01-19 13:07:50 +0100

    database changes

--- server.js
+++ server.js
@@ -12,9 +12,7 @@ const image = require('./controllers/image.js')
 const db = knex({
     client: 'pg',
     connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false
-    }
+    ssl: true
   });
 
 const app = express();
