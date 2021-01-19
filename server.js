d1f5078645a23eb4c5002fe0cff1f84db5643b5b
Jugo-JS <jugoslavsubotic@gmail.com>
2021-01-19 09:50:29 +0100

    adding ports

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ app.use(bodyParser.json());
 
 app.use(cors());
 
+app.get('/', (req, res) => { res.send('it it working') })
 
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
 
@@ -37,6 +38,6 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
-app.listen(3000, () => {
-    console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`app is running on port ${process.env.PORT}`);
 })
